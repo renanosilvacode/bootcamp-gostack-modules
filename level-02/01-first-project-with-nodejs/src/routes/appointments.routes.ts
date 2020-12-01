01e608dcfc3de172993e1721eb159df98b15455e
Renan Oliveira da Silva <renanosilva@gmail.com>
2020-12-01 00:11:10 -0300

    GetAll Appointments

--- level-02/01-first-project-with-nodejs/src/routes/appointments.routes.ts
+++ level-02/01-first-project-with-nodejs/src/routes/appointments.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { request, response, Router } from 'express';
 import { startOfHour, parseISO } from 'date-fns';
 
 
@@ -6,6 +6,12 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+})
 
 appointmentsRouter.post('/', (request, response) => {
 
