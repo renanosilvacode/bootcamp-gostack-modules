88e8677391f583c16c63c3e40567562647b7299f
Renan Oliveira da Silva <renanosilva@gmail.com>
2020-12-01 00:03:12 -0300

    Creating repository

--- level-02/01-first-project-with-nodejs/src/routes/appointments.routes.ts
+++ level-02/01-first-project-with-nodejs/src/routes/appointments.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual, parse } from 'date-fns';
-import Appointment from '../models/Appointments';
+import { startOfHour, parseISO } from 'date-fns';
+
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (request, response) => {
 
@@ -12,17 +13,13 @@ appointmentsRouter.post('/', (request, response) => {
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date)
-  )
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
 
   if(findAppointmentInSameDate){
     return response.status(400).json({message:'This appointment is already booked'});
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
