420409ea36a0fde47ec39f3c600d3ae919d70672
Renan Oliveira da Silva <renanosilva@gmail.com>
2020-12-14 23:03:36 -0300

    Creating token JWT

--- level-02/01-first-project-with-nodejs/src/service/AuthenticateUserService.ts
+++ level-02/01-first-project-with-nodejs/src/service/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign, verify } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response{
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService{
@@ -30,8 +32,14 @@ class AuthenticateUserService{
       throw new Error('Incorrect email/password combination');
     }
 
+    const token = sign({}, 'e89292fc12331534f2850be87a816e9a', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return{
       user,
+      token,
     };
 
   }
