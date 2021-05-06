1aa5d2ed5bb9f543f4309243325ae14d328e6c9e
Renan Oliveira da Silva <renanosilva@gmail.com>
2021-05-05 22:57:21 -0300

    Dashboard Styled-Components

--- level-03/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ level-03/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles'
+import logoImg from '../../assets/logo.svg'
+
+import { Title, Form, Repositories } from './styles'
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no Github</Title>
+    return(
+    <>
+        <img src={logoImg} alt="Github Explorer" />
+        <Title>Explore repositórios no Github</Title>
+
+        <Form>
+            <input placeholder="Digite o nome do repositório"/>
+            <button type="submit">Pesquisar</button>
+        </Form>
+
+        <Repositories>
+            <a href="teste">
+                <img src="https://avatars.githubusercontent.com/u/25445892?v=4" alt="Renan Oliveira"/>
+                <div>
+                    <strong>renanosilvacode/proffy</strong>
+                    <p>sample repository</p>
+                </div>
+
+                <FiChevronRight size={20} />
+            </a>
+            <a href="teste">
+                <img src="https://avatars.githubusercontent.com/u/25445892?v=4" alt="Renan Oliveira"/>
+                <div>
+                    <strong>renanosilvacode/proffy</strong>
+                    <p>sample repository</p>
+                </div>
+
+                <FiChevronRight size={20} />
+            </a>
+            <a href="teste">
+                <img src="https://avatars.githubusercontent.com/u/25445892?v=4" alt="Renan Oliveira"/>
+                <div>
+                    <strong>renanosilvacode/proffy</strong>
+                    <p>sample repository</p>
+                </div>
+
+                <FiChevronRight size={20} />
+            </a>
+        </Repositories>
+    </>
+    ) 
 }
 
 export default Dashboard;
