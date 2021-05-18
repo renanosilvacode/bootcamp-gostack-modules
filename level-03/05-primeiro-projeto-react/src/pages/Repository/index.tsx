14c9a0ebd77beb34e6812e323fbeebabcb220935
Renan Oliveira da Silva <renanosilva@gmail.com>
2021-05-18 12:17:33 -0300

    Listing API Issues

--- level-03/05-primeiro-projeto-react/src/pages/Repository/index.tsx
+++ level-03/05-primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import api  from '../../services/api';
@@ -10,12 +10,43 @@ interface RepositoryParams{
     repository: string;
 }
 
+interface Repository {
+    full_name: string;
+    description: string;
+    stargazers_count: number;
+    forks_count: number;
+    open_issues_count: number;
+    owner: {
+        login: string;
+        avatar_url: string;
+    }
+
+}
+
+interface Issue {
+    id: number;
+    title: string;
+    html_url: string;
+    user: {
+        login: string;
+    }
+}
+
 const Repository: React.FC = () => {
+    const [repository, setRepository] = useState<Repository | null>(null);
+    const [issues, setIssues] = useState<Issue[]>([]);
+
     const{ params } = useRouteMatch<RepositoryParams>();
 
     useEffect(() => {
-        
-    }, []);
+        api.get(`repos/${params.repository}`).then(response => {
+            setRepository(response.data);
+        });
+
+        api.get(`repos/${params.repository}/issues`).then(response => {
+            setIssues(response.data);
+        })
+    }, [params.repository]);
 
     return(
         <>
@@ -27,39 +58,45 @@ const Repository: React.FC = () => {
                 </Link>
             </Header>
 
-            <RepositoryInfo>
-                <header>
-                    <img src={logoImg} alt="Descricao" />
-                    <div>
-                        <strong>repositorio/nome</strong>
-                        <p>descricao repo</p>
-                    </div>
-                </header>
-                <ul>
-                    <li>
-                        <strong>1800</strong>
-                        <span>Stars</span>
-                    </li>
-                    <li>
-                        <strong>1800</strong>
-                        <span>Forks</span>
-                    </li>
-                    <li>
-                        <strong>1800</strong>
-                        <span>Issues</span>
-                    </li>
-                </ul>
-            </RepositoryInfo>
+            {repository && (
+                <RepositoryInfo>
+                    <header>
+                        <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                    </header>
+                    <ul>
+                        <li>
+                            <strong>{repository.stargazers_count}</strong>
+                            <span>Stars</span>
+                        </li>
+                        <li>
+                            <strong>{repository.forks_count}</strong>
+                            <span>Forks</span>
+                        </li>
+                        <li>
+                            <strong>{repository.open_issues_count}</strong>
+                            <span>Issues</span>
+                        </li>
+                    </ul>
+                </RepositoryInfo>
+
+            )}
 
             <Issues>
-                <Link to="asasd">
-                    <div>
-                        <strong>full_name</strong>
-                        <p>description</p>
-                    </div>
+                {issues.map(issue => (
 
-                    <FiChevronRight size={20} />
-                </Link>
+                    <a key={issue.id} href={issue.html_url} >
+                        <div>
+                            <strong>{issue.title}</strong>
+                            <p>{issue.user.login}</p>
+                        </div>
+
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
             </Issues>
         </>
     )
