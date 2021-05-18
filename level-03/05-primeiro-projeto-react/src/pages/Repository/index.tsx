40e2355aa411d66c6edaf49621f74253eb412ff9
Renan Oliveira da Silva <renanosilva@gmail.com>
2021-05-18 11:45:01 -0300

    Styling Repository Detail

--- level-03/05-primeiro-projeto-react/src/pages/Repository/index.tsx
+++ level-03/05-primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import api  from '../../services/api';
 
+import logoImg from '../../assets/logo.svg'
+
+import { Header, RepositoryInfo, Issues } from './styles'
 interface RepositoryParams{
     repository: string;
 }
@@ -8,7 +13,56 @@ interface RepositoryParams{
 const Repository: React.FC = () => {
     const{ params } = useRouteMatch<RepositoryParams>();
 
-    return <h1>Repository:{params.repository}</h1>
+    useEffect(() => {
+        
+    }, []);
+
+    return(
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer"/>
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src={logoImg} alt="Descricao" />
+                    <div>
+                        <strong>repositorio/nome</strong>
+                        <p>descricao repo</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1800</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>1800</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>1800</strong>
+                        <span>Issues</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link to="asasd">
+                    <div>
+                        <strong>full_name</strong>
+                        <p>description</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    )
 }
 
 export default Repository;
